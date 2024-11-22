504bab835d62780d719c0f14d6e061383b69cddc
Siddhesh Sule <64964929+siddheshsule@users.noreply.github.com>
2024-11-22 17:10:23 +0100

    Initial commit

---
+++ components/Hero.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+const Hero = () => {
+  return <div>Hero</div>;
+};
+export default Hero;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,34 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      fontFamily: {
+        zentry: ['zentry', 'sans-serif'],
+        general: ['general', 'sans-serif'],
+        'robert-medium': ['robert-medium', 'sans-serif'],
+        'robert-regular': ['robert-regular', 'sans-serif'],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        blue: {
+          50: '#DFDFF0',
+          75: '#DFDFF2',
+          100: '#F0F2FA',
+          200: '#010101',
+          300: '#4FB7DD',
+        },
+        violet: {
+          300: '#5724FF',
+        },
+        yellow: {
+          100: '#8E983F',
+          300: '#EDFF66',
+        },
       },
     },
   },
