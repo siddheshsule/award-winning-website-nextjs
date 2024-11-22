0e23e639c8eb5068a0e093b06374315136702bc8
Siddhesh Sule <64964929+siddheshsule@users.noreply.github.com>
2024-11-22 22:07:46 +0100

    Labels and button added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useRef, useState } from "react";
+import Button from "./Button";
+import { TiLocationArrow } from "react-icons/ti";
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -58,15 +60,43 @@ const Hero = () => {
             onLoadedData={handleVideoLoad}
           />
 
-          <video src= {getVideoSource(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
-            autoPlay
+          <video
+            src={getVideoSource(
+              currentIndex === totalVideos - 1 ? 1 : currentIndex
+            )}
+            // autoPlay
             loop
             muted
             className="absolute left-0 top-0 size-full object-cover object-center"
-          
           />
         </div>
+
+        <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
+          G<b>a</b>ming
+        </h1>
+
+        <div className="absolute left-0 top-0 z-40 size-full">
+          <div className="mt-24 px-5 sm:px-10">
+            <h1 className="special-font hero-heading text-blue-100">
+              redefi<b>n</b>e
+            </h1>
+            <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
+              Enter the Metagame layer <br />
+              Unleash the Play Economy
+            </p>
+            <Button
+              id="watch-trailer"
+              title="Watch Trailer"
+              leftIcon={<TiLocationArrow />}
+              containerClass="!bg-yellow-300 flex-center gap-1"
+            />
+          </div>
+        </div>
       </div>
+
+      <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
+        G<b>a</b>ming
+      </h1>
     </div>
   );
 };
