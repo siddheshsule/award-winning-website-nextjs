db84b47c683dc2bbaf1dce791c7502428673ebdc
Siddhesh Sule <64964929+siddheshsule@users.noreply.github.com>
2024-11-22 19:13:29 +0100

    Prettier Eslint removed,they are just annoying!

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,34 @@
-import React from 'react';
+'use client';
+
+import { useRef, useState } from 'react';
+
 const Hero = () => {
-  return <div>Hero</div>;
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const [hasClicked, setHasClicked] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadingVideos, setLoadingVideos] = useState(0);
+
+  const totalVideos = 4;
+  const nextVdRef = useRef(null);
+
+  const handleMiniVidClick = () => {
+    setHasClicked(true);
+    setCurrentIndex((prev) => prev + 1);
+  };
+
+  return (
+    <div className="relative h-dvh w-screen overflow-x-hidden">
+      <div
+        id="video-frame"
+        className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
+      >
+        <div>
+          <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+            <div>MiniVideoPlayer</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Hero;
