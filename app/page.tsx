ebcbca7ff30484da717827345b738f80b5d544be
Siddhesh Sule <64964929+siddheshsule@users.noreply.github.com>
2024-11-26 17:44:36 +0100

    Story implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import About from '@/components/About';
+import Contact from '@/components/Contact';
 import Features from '@/components/Features';
+import Footer from '@/components/Footer';
 import Hero from '@/components/Hero';
 import Navbar from '@/components/Navbar';
+import Story from '@/components/Story';
 
 export default function Home() {
   return (
@@ -10,6 +13,10 @@ export default function Home() {
       <Hero />
       <About />
       <Features />
+      <Story />
+      <Contact />
+      <Footer />
+      
     </div>
   );
 }
